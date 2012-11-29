4d0e8106b7bdd7246d6c888c7370c72367f3c2ae
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2012-11-28 22:22:36 -0500

    Used both loops

--- StrunkM.js
+++ StrunkM.js
@@ -5,20 +5,41 @@
 
 
 
-// My variables
-var myHeroName = [
+// My variables in array
+var myHeroNames = [
 	"Bruno",
 	"Axel",
 	"Rose"
 ];
 
+var weaponsChoice = [
+	"Axe",
+	"Sword",
+	"Bow"
+];
+
 // While loop
 var floor = 0;
 while (floor < 4) {
-	console.log (myHeroName[1] + " searched  floor number " + floor +" and did not find the dragon ");
+	console.log (myHeroNames + " all searched  floor number " + floor +" and did not find the dragon ");
 	floor++;
 };
-console.log ("Found the dragon on the 4th floor!")
+console.log ("They found the dragon on the 4th floor!")
+
+// for-loop
+
+for (var weaponChoice = 0; weaponChoice < weaponsChoice.length; weaponChoice++){
+	var myHeroName = myHeroNames[weaponsChoice],
+		weaponThisTime = weaponsChoice[weaponChoice];
+	
+	
+	console.log(myHeroNames + " chose to use the " + weaponsChoice + " in respective order. ")
+	
+};
+
+
+
+
 
 
 
