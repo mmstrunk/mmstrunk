beed2810051d3b574d9f0df59472f1763d4882c2
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2012-11-28 20:47:34 -0500

    Added a array

--- StrunkM.js
+++ StrunkM.js
@@ -3,42 +3,26 @@
 // Project 1
 // My first JavaScript
 
-// My variables
-var myHeroName = "Bruno";
-var castleLocation = "Red Castle";
-var weaponChoice = "Bow";
 
+
+// My variables
+var myHeroName = [
+	"Bruno",
+	"Axel",
+	"Rose"
+];
+
+// While loop
 var floor = 0;
 while (floor < 4) {
-	console.log (" searched " + floor +" and did not find the dragon ");
+	console.log (myHeroName[1] + " searched  floor number " + floor +" and did not find the dragon ");
 	floor++;
 };
+console.log ("Found the dragon on the 4th floor!")
 
-console.log ("Found the dragon")
 
-//My outputs
-console.log ("The hero's name is " + myHeroName);
-console.log ("He is trying to slay the dragon and save the princess")
-console.log ("He used his " + weaponChoice + " as weapon of choice");
-console.log (myHeroName + " traveled to the  " + castleLocation);
 
-// Conditional 1
-if (castleLocation === "Blue Castle") {
-    console.log(myHeroName + " did not find the castle and would have to search another day");
-}
-    else {
-    if (castleLocation === "Red Castle") {
-    console.log("He found the castle that was harboring the Dragon");
-}
-// Conditinal 2
-if (weaponChoice === "Sword") {
-    console.log(myHeroName + " was defeated by the dragon");
-}
- else {
-    if (weaponChoice === "Bow") 
-    console.log("The Dragon was defeated in distant combat")
-    console.log("He recieved a kiss from the princess")
- }}
+
 
  
 
