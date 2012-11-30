891068f8bd54fb4a8624340919a9c3ffc25ff3f1
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2012-11-29 20:52:33 -0500

    Final

--- StrunkM.js
+++ StrunkM.js
@@ -18,25 +18,31 @@ var weaponsChoice = [
 	"Bow"
 ];
 
+// function
+var say = function(message){ console.log(message); 
+	
+};
+
 // While loop
-var floor = 0;
-while (floor < 4) {
-	console.log (myHeroNames + " all searched  floor number " + floor +" and did not find the dragon ");
+	var floor = 0;
+		while (floor < 4) {
+	
+	say(myHeroNames + " all searched  floor number " + floor +" and did not find the dragon ");
+	
 	floor++;
 };
-console.log ("They found the dragon on the 4th floor!")
+	say("They found the dragon on the 4th floor!")
 
 // for-loop
-
-for (var i = 0, j = weaponsChoice.length; i < j; i++){
+	for (var i = 0, j = weaponsChoice.length; i < j; i++){
 	var myHeroName = myHeroNames[i];
 		weaponThisTime = weaponsChoice[i];
 	
-	
-	console.log(myHeroName + " chose to use the " + weaponThisTime + " as his weapon. ")
+	say(myHeroName + " chose to use the " + weaponThisTime + " as his weapon. ")
 	
 };
 
+say( "The dragon slayed " + myHeroNames + " with one breath of fire! ")
 
 
 
