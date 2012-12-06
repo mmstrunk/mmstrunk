341aa24671a55e2c5674591ea7abdf575262773c
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2012-12-06 14:10:46 -0500

    with json not working

---
+++ Json.js
@@ -0,0 +1,18 @@
+// My json data
+
+var json = {
+	"fallenHeros": [
+		{
+			"Name": "Link",
+			"Age": 26	
+		},
+		{
+			"Name": "Frotto",
+			"Age": 114	
+		},
+		{
+			"Name": "Bilbo",
+			"Age": 111	
+		}
+	]
+};
