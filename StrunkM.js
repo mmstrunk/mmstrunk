22967213ee2a2d7a22f031958274b4f7174a899e
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2012-12-15 14:37:05 -0500

    Phone Number working

--- StrunkM.js
+++ StrunkM.js
@@ -1,114 +1,23 @@
 // Michael Strunk
 // SDI 1212
-// Project 3
-// My third JavaScript
+// Project 4
+// Code Library
 
-
-// Hero Keys
-
-var Bruno = {
-	age: "28",
-	height: "Six Foot",
-	catchPrase: "Im here to stay",
-	killDragon: false
-};
-
-var Axel = {
-	age: "34",
-	height: "Six Two",
-	catchPrase: "Time to die",
-	killDragon: false
-};
-
-var Rose = {
-	age: "24",
-	height: "Five two",
-	catchPrase: "This looks like a job for a girl",
-	killDragon: true
-};
-
-
-// My variables
-var myHeroNames = [
-	"Bruno",
-	"Axel",
-	"Rose"
-];
-
-var weaponsChoice = [
-	"Axe",
-	"Sword",
-	"Bow"
-];
-
-// construtor
-var prize = function (amount){
-	var dragonDrop = [];
-	var droped = function (item){
-		dragonDrop.push(item);
-	};
+var StringLibrary = function (str) {
+	
+	// Checks for a phone number
+	// Returns boolean
+	var isPhoneNum = function (str) {
+		var re = /\d{3}-\d{3}-\d{4}/;
+		return re.test(str);
+		};
+	
 	return {
-		"amount": amount,
-		"fellBehind": dragonDrop,
-		"droped": droped
+			"isPhoneNum" : isPhoneNum
 	};
+			
 };
 
-var gold = prize("Gold");
 
-// function
-var say = function(message){ console.log(message); 
-	
-};
-
-// While loop
-	var floor = 0;
-		while (floor < 4) {
-	
-	say(myHeroNames + " all searched  floor number " + floor +" and did not find the dragon ");
-	
-	floor++;
-};
-	say("They found the dragon on the 4th floor!")
-
-// for-loop
-	for (var i = 0, j = weaponsChoice.length; i < j; i++){
-	var myHeroName = myHeroNames[i];
-		weaponThisTime = weaponsChoice[i];
-	
-	say(myHeroName + " chose to use the " + weaponThisTime + " as his weapon. ")
-	
-};
-
-// json for-loop
-
-var goneHeros = function (JSON) {
-	for (var i = 0; i < json.fallenHeros.length; i++){
-		var deadHero = json.fallenHeros[i];
-		say(" At the age of " + deadHero.age + ", the hero " + deadHero.name + " was slane by the dragon");
-	};
-};
-
-if (Bruno.killDragon === true) {
-	say("Bruno defeated the dragon!")
-} else {
-	say("The Dragon slayed Bruno")
-};
-
-if (Axel.killDragon === true) {
-	say("Axel defeated the dragon!")
-} else {
-	say("The Dragon slayed Axel")
-};
-
-if (Rose.killDragon === true) {
-	say("Rose defeated the dragon!")
-} else {
-	say("The Dragon slayed Rose")
-};
-
-say("The dragon dropped gold arrows");
-
-gold.droped("gold arrows")
-
-say(gold + " was added to Roses invitory");
+var stringLib = StringLibrary();
+console.log(stringLib.isPhoneNum("941-244-0011"));
